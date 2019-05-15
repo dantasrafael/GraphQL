3d20c8b2df07b78e60d026727006562789145aaf
Rafael Dantas <dantasrafael@yahoo.com.br>
2019-05-14 21:21:21 -0300

    Initial commit

---
+++ schema-query/projeto/index.js
@@ -0,0 +1,54 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    scalar Date
+
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+    
+    # Pontos de entradas de sua API
+    type Query {
+        ola: String!
+        horaAtual: Date!
+        usuarioLogado: Usuario
+    }
+
+`
+
+const resolvers = {
+    Query: {
+        ola() {
+            return 'Bom dia!';
+        },
+        horaAtual() {
+            return new Date;
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Ana da Web',
+                email: 'anadaweb@email.com',
+                idade: 23,
+                salario: 1234.56,
+                vip: true
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+
+server.listen().then(({ url }) => {
+    console.log(`Execuntado em ${url}`);
+    
+})
