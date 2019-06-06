649588bef5900b0f0604c361c585a2d1db36e664
Rafael Dantas <rafael.d@softplan.com.br>
2019-06-06 14:26:45 -0300

    consultas e inserts com knex

--- banco-de-dados/projeto/testes/insert.js
+++ banco-de-dados/projeto/testes/insert.js
@@ -1,12 +1,23 @@
-const db = require('../config/db')
+ const db = require('../config/db')
 
-const novoPerfil = {
-    nome: 'visitante',
-    rotulo: 'Visitante'
+// const novoPerfil = {
+//     nome: 'visitante',
+//     rotulo: 'Visitante'
+// }
+
+// db('perfis')
+//     .insert(novoPerfil)
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err.sqlMessage))
+//     .finally(() => db.destroy())
+
+const perfilSU = {
+    nome: 'root' + Math.random(),
+    rotulo: 'Super Usuário'
 }
 
-db('perfis')
-    .insert(novoPerfil)
-    .then(res => console.log(res))
-    .catch(err => console.log(err.sqlMessage))
-    .finally(() => db.destroy())
+ db('perfis')
+     .insert(perfilSU)
+     .then(res => console.log(res))
+     .catch(err => console.log(err.sqlMessage))
+     .finally(() => db.destroy())
