72d1858a391f21d424f9e2a88b676bdb2512f2e4
Rafael Dantas <rafael.d@softplan.com.br>
2019-06-13 13:33:17 -0300

    Desafio final 1 parte - Querys

--- banco-de-dados/desafio/knexfile.js
+++ banco-de-dados/desafio/knexfile.js
@@ -1,12 +1,9 @@
-// Update with your config settings.
+const { connection } = require('./.env')
 
 module.exports = {
-  client: 'mysql',
-  connection: {
-    database: 'desafio-cap-04',
-    user:     'root',
-    password: '12345678'
-  },
+
+  client: 'postgresql',
+  connection,
   pool: {
     min: 2,
     max: 10
