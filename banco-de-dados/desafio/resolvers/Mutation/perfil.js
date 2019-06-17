63bc36ca22c63491a69d9891ecd3dd0413f00500
Rafael Dantas <rafael.d@softplan.com.br>
2019-06-17 13:44:01 -0300

    mutations perfil e usuario

--- banco-de-dados/desafio/resolvers/Mutation/perfil.js
+++ banco-de-dados/desafio/resolvers/Mutation/perfil.js
@@ -1,13 +1,38 @@
 const db = require('../../config/db')
+const { perfil: obterPerfil } = require('../Query/perfil')
 
 module.exports = {
     async novoPerfil(_, { dados }) {
-        // implementar
+        try {
+            await db('perfis').insert(dados)
+            return await db('perfis').where({ nome: dados.nome }).first()
+        } catch (e) {
+            throw new Error(e);
+        }
     },
     async excluirPerfil(_, { filtro }) {
-        // implementar
+        try {
+            const perfil = await obterPerfil(_, { filtro })
+            if (perfil) {
+                const { id } = perfil
+                await db('usuario_perfil').where({ perfil_id: id }).delete()
+                await db('perfis').where({ id }).delete()
+            }
+            return perfil;
+        } catch (e) {
+            throw new Error(e);
+        }
     },
     async alterarPerfil(_, { filtro, dados }) {
-        // implementar
+        try {
+            const perfil = await obterPerfil(_, { filtro })
+            if (perfil) {
+                const { id } = perfil
+                await db('perfis').where({ id }).update(dados)
+            }
+            return { ...perfil, ...dados };
+        } catch (e) {
+            throw new Error(e);
+        }
     }
 }
--- banco-de-dados/desafio/resolvers/Mutation/usuario.js
+++ banco-de-dados/desafio/resolvers/Mutation/usuario.js
@@ -1,13 +1,65 @@
 const db = require('../../config/db')
+const { perfil: obterPerfil } = require('../Query/perfil')
+const { usuario: obterUsuario } = require('../Query/usuario')
 
 module.exports = {
     async novoUsuario(_, { dados }) {
-        // Implementar
+        try {
+            const idsPerfis = [];
+            if (dados.perfis) {
+                for (let filtro of dados.perfis) {
+                    const perfil = await obterPerfil(_, { filtro });
+                    if (perfil) {
+                        idsPerfis.push(perfil.id);
+                    } 
+                }
+            }
+
+            delete dados.perfis;
+            await db('usuarios').insert({ ...dados });
+            const usuario = await db('usuarios').where({ nome: dados.nome }).first();
+
+            for (let perfil_id of idsPerfis) {
+                await db('usuario_perfil').insert({ perfil_id, usuario_id:usuario.id });
+            }
+
+            return usuario;
+        } catch (e) {
+            throw new Error(e);
+        }
     },
     async excluirUsuario(_, { filtro }) {
-        // Implementar
+        try {
+            const usuario = await obterUsuario(_, { filtro })
+            if (usuario) {
+                const { id } = usuario
+                await db('usuario_perfil').where({ usuario_id: id }).delete()
+                await db('usuarios').where({ id }).delete()
+            }
+            return usuario;            
+        } catch (e) {
+            throw new Error(e);
+        }
     },
     async alterarUsuario(_, { filtro, dados }) {
-        // Implementar
+        try {
+            const usuario = await obterUsuario(_, { filtro })
+            if (usuario) {
+                const { id } = usuario
+                if (dados.perfis) {
+                    await db('usuario_perfil').where({ usuario_id: id }).delete();
+                    for (let filtro of dados.perfis) {
+                        const perfil = await obterPerfil(_, { filtro });
+                        perfil && await db('usuario_perfil').insert({ perfil_id: perfil.id, usuario_id:id });
+                    }
+                }
+                
+                delete dados.perfis;
+                await db('usuarios').where({ id }).update(dados);
+            }
+            return !usuario ? null : { ...usuario, ...dados };
+        } catch (e) {
+            throw new Error(e);
+        }
     }
 }
