a2130e8bed35fe2e4428a1c353ca1f24db0396a8
Rafael Dantas <rafael.d@softplan.com.br>
2019-06-18 12:45:08 -0300

    Registrar Usuario

--- projeto-final/backend/resolvers/Query/usuario.js
+++ projeto-final/backend/resolvers/Query/usuario.js
@@ -1,6 +1,21 @@
 const db = require('../../config/db')
+const bcrypt = require('bcrypt-nodejs')
+const { getUsuarioLogado } = require('../Comum/usuario')
 
 module.exports = {
+    async login(_, { dados }) {
+        const usuario = await db('usuarios').where({ email: dados.email }).first()
+        if(!usuario) {
+            throw new Error('Usuário não encontrado')
+        }
+        
+        const saoIguais = bcrypt.compareSync(dados.senha, usuario.senha)
+        if(!saoIguais) {
+            throw new Error('Usuário/Senha inválido')
+        }
+
+        return getUsuarioLogado(usuario)
+    },
     usuarios() {
         return db('usuarios')
     },
