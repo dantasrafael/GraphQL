25b92882fbcad787c3ff02a3aacf0a61b17c4cd3
Rafael Dantas <dantasrafael@yahoo.com.br>
2019-06-17 21:22:29 -0300

    registrar usuario

--- projeto-final/backend/knexfile.js
+++ projeto-final/backend/knexfile.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 
 module.exports = {
-  client: 'postgresql',
-  connection,
+  client: process.env.APP_DB_CLIENT,
+  connection: {
+    host: process.env.APP_DB_HOST,
+    database: process.env.APP_DB_DATABASE,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASS,
+    ssl: true
+  },
   pool: {
     min: 2,
     max: 10
--- projeto-final/backend/resolvers/Mutation/usuario.js
+++ projeto-final/backend/resolvers/Mutation/usuario.js
@@ -1,31 +1,45 @@
+const bcrypt = require('bcrypt-nodejs')
 const db = require('../../config/db')
 const { perfil: obterPerfil } = require('../Query/perfil')
 const { usuario: obterUsuario } = require('../Query/usuario')
 
-module.exports = {
+const mutations = {
+    registrarUsuario(_, { dados }) {
+        return mutations.novoUsuario(_, {
+            dados: {
+                nome: dados.nome,
+                email: dados.email,
+                senha: dados.senha
+            }
+        })
+    },
     async novoUsuario(_, { dados }) {
         try {
             const idsPerfis = []
-            if(dados.perfis) {
-                for(let filtro of dados.perfis) {
-                    const perfil = await obterPerfil(_, {
-                        filtro
-                    })
-                    if(perfil) idsPerfis.push(perfil.id)
-                }
+            if (!dados.perfis || !dados.perfis.length) {
+                dados.perfis = [
+                    { nome: 'comum' }
+                ]
             }
+
+            for(let filtro of dados.perfis) {
+                const perfil = await obterPerfil(_, { filtro })
+                if(perfil) idsPerfis.push(perfil.id)
+            }
+
+            // cript da senha
+            const salt = bcrypt.genSaltSync()
+            dados.senha = bcrypt.hashSync(dados.senha, salt)
 
             delete dados.perfis
-            const [ id ] = await db('usuarios')
-                .insert(dados)
+            await db('usuarios').insert(dados)
+            const usuario = await db('usuarios').where({ email: dados.email }).first()
 
             for(let perfil_id of idsPerfis) {
-                await db('usuarios_perfis')
-                    .insert({ perfil_id, usuario_id: id })
+                await db('usuarios_perfis').insert({ perfil_id, usuario_id: usuario.id })
             }
 
-            return db('usuarios')
-                .where({ id }).first()
+            return usuario
         } catch(e) {
             throw new Error(e.sqlMessage)
         }
@@ -35,10 +49,8 @@ module.exports = {
             const usuario = await obterUsuario(_, args)
             if(usuario) {
                 const { id } = usuario
-                await db('usuarios_perfis')
-                    .where({ usuario_id: id }).delete()
-                await db('usuarios')
-                    .where({ id }).delete()
+                await db('usuarios_perfis').where({ usuario_id: id }).delete()
+                await db('usuarios').where({ id }).delete()
             }
             return usuario
         } catch(e) {
@@ -52,8 +64,7 @@ module.exports = {
             if(usuario) {
                 const { id } = usuario
                 if(dados.perfis) {
-                    await db('usuarios_perfis')
-                        .where({ usuario_id: id }).delete()
+                    await db('usuarios_perfis').where({ usuario_id: id }).delete()
 
                     for(let filtro of dados.perfis) {
                         const perfil = await obterPerfil(_, {
@@ -61,8 +72,7 @@ module.exports = {
                         })
                         
                         if(perfil) {
-                            await db('usuarios_perfis')
-                                .insert({
+                            await db('usuarios_perfis').insert({
                                     perfil_id: perfil.id,
                                     usuario_id: id
                                 })
@@ -70,10 +80,14 @@ module.exports = {
                     }
                 }
 
+                // cript da senha
+                if (dados.senha) {
+                    const salt = bcrypt.genSaltSync()
+                    dados.senha = bcrypt.hashSync(dados.senha, salt)
+                }
+
                 delete dados.perfis
-                await db('usuarios')
-                    .where({ id })
-                    .update(dados)
+                await db('usuarios').where({ id }).update(dados)
             }
             return !usuario ? null : { ...usuario, ...dados }
         } catch(e) {
@@ -81,3 +95,5 @@ module.exports = {
         }
     }
 }
+
+module.exports = mutations
