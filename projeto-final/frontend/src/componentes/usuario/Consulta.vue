90c211381445954a58e57ec431cfd1e324d211d8
Rafael Dantas <dantasrafael@yahoo.com.br>
2019-06-23 22:41:11 -0300

    lista e consulta de usuarios

--- projeto-final/frontend/src/componentes/usuario/Consulta.vue
+++ projeto-final/frontend/src/componentes/usuario/Consulta.vue
@@ -40,6 +40,7 @@
 
 <script>
 import Erros from '../comum/Erros'
+import gql from 'graphql-tag'
 
 export default {
     components: { Erros },
@@ -59,7 +60,34 @@ export default {
     },
     methods: {
         consultar() {
-            // implementar
+            this.$api.query({
+                query: gql`
+                    query(
+                        $id: Int
+                        $email: String
+                    ) { 
+                        usuario(
+                            filtro: {
+                                id: $id
+                                email: $email
+                            }
+                        ) {
+                             id nome email perfis { rotulo } 
+                        }
+                    }
+                `,
+                fetchPolicy: 'network-only',
+                variables: {
+                    id: this.filtro.id,
+                    email: this.filtro.email
+                }
+            }).then(resultado => {
+                this.dados = resultado.data.usuario
+                this.erros = null
+                this.filtro = {}
+            }).catch(e => {
+                this.erros = e
+            })
         }
     }
 }
